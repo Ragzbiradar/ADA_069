427d40880fbfa835e1e348d634261921753d4881
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2019-08-14 12:24:05 +0530

    Add files via upload

---
+++ sqrtsearch.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class sqrtsearch{
+	int s=0,e,mid,n,sqrt;
+	public:
+	sqrtsearch(int a){
+		n=a;
+		e=n;
+	}
+	int square(){
+		while(s<=e){
+			mid=(s+e)/2;
+			int sqr=mid*mid;
+			if(sqr==n){
+				sqrt=mid;
+				break;
+			}
+			else if (sqr<n){
+				s=mid+1;
+				sqrt=mid;
+			}
+			else if (sqr>n){
+				e=mid-1;
+			}
+		}
+		return sqrt;
+	}
+};
+int main(){
+	int number,root;
+	cout<<"enter the number";
+	cin>>number;
+	sqrtsearch s(number);
+	root=s.square();
+	cout<<"root is "<<root;
+	return 0;
+}
