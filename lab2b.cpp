78f57a771197833a8b9a0f402a173ff5a116a0d2
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2019-08-20 21:25:41 +0530

    k largest elements using bubble sort

---
+++ lab2b.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class klarge{
+      int arr[100],k,n;
+public:
+    void input(){
+        cout<<"enter the size of array \n";
+        cin>>n;
+        cout<<"enter the array elements\n";
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        cout<<"enter the value of k =";
+        cin>>k;
+    }
+    void bblsort(){
+        int i,j,temp;
+        for(i=0;i<=k-1;i++){
+            for(j=0;j<=n-2-i;j++){
+                if(arr[j+1]<arr[j]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+            cout<<arr[n-i-1]<<"\n";
+        }
+
+
+    }
+};
+int main(){
+    klarge kl;
+    kl.input();
+    kl.bblsort();
+    return 0;
+}
