c700dcfffa8b7077bd716d6520a7f2831f59c92d
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2019-09-03 22:33:27 +0530

    counting the number of islands in a input matrix

---
+++ lab4b.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#define n1 3
+#define n2 5
+using namespace std;
+    int M[n1][n2];
+    bool visited[n1][n2];
+
+        void input(){
+        //cout<<"enter the no. of rows \n";
+        //cin>>n1;
+        //cout<<"columns:";
+        //cin>>n2;
+        for (int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+            visited[i][j]=false;
+        }
+        }
+        cout<<"enter the input matrix of graph\n";
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                cin>>M[i][j];
+            }}
+       }
+       int isSafe(int M[][n2],int row,int col,bool visited[][n2]){
+           return ((row>=0)&&(row<n1)&&(col>=0)&&(col<n2)&&(M[row][col]&& !visited[row][col]));
+       }
+       void DFS(int M[][n2],int row,int col,bool visited[][n2]){
+           static int rowNbr[]={-1,-1,-1,0,0,1,1,1};
+           static int colNbr[]={-1,0,1,-1,1,-1,0,1};
+           visited[row][col]=true;
+           for(int k=0;k<8;k++){
+            if(isSafe(M,row+rowNbr[k],col+colNbr[k],visited)){
+                DFS(M,row+rowNbr[k],col+colNbr[k],visited);
+            }
+           }
+       }
+       int countIslands(int M[][n2]){
+           int count=0;
+           for(int i=0;i<n1;++i){
+            for(int j=0;j<n2;++j){
+                if(M[i][j] && !visited[i][j]){
+                DFS(M,i,j,visited);
+                ++count;
+                }
+            }
+           }
+           return count;
+
+       }
+
+
+int main(){
+//countisland c;
+input();
+int n=countIslands(M);
+cout<<n<<" islands are present";
+return 0;
+}
+
